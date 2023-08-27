9cbad6a9124a802caa1c47b4305aa0ae3ac143b2
rakko <124496617+Rakkochchch@users.noreply.github.com>
2023-08-27 19:27:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -15,54 +15,61 @@ function typede(typedeid,type_name){
     
     }
 
-
 typede("#typed-sample","ここは　平和で　明るい国　きょうも　ゆかいな仲間たちが　たのしくせいかつをしている")
-typede("#typed-sample2","はずだったのだが。。。")
-typede("#typed-sample3","国を　ほうかい　させるべく　4体の魔物が　国に侵入してきた　のだった")
+function type2in(){
+    typede("#typed-sample2","はずだったのだが。。。")
+}
+function type3in(){
+    typede("#typed-sample3","国を　ほうかい　させるべく　4体の魔物が　国に侵入してきた　のだった")
+}
+function type4in(){
+   typede("#typed-sample4","こうなったら　平和も何も　あったもんじゃない　きょうあくな　魔物は　すきかってに　あばれだし　芸付は　さながら　むほうちたいとなってしまった")
+ 
+}
+function type5in(){
+    typede("#typed-sample5","そんなじゅうみんたちの　たったひとつのきぼうは　がくえんないにちらばらる　４つの封印スタンプだった　それさえあれば　ふたたび　平和をとりもどす　ことができるのだ　しかしどこに　まただれが　そのスタンプをさがしにいくのか？")
+  
+}
+function type6in(){
+    typede("#typed-sample6","みんながそうおもったとき　3にんの　ゆうきあるせんしがたちあがった！！　そのなを　ゆうしゃ　ヒーラー　魔法使い　\nはたしてかれらは　ぶじ　スタンプを　さがしだすことができるのであろうか？")
 
-typede("#typed-sample5","こうなったら　平和も何も　あったもんじゃない　\nきょうあくな　魔物は　すきかってに　あばれだし　芸付は　さながら　むほうちたいとなってしまった")
-typede("#typed-sample6","そんなじゅうみんたちの　たったひとつのきぼうは　がくえんないにちらばらる　４つの封印のスタンプだった　それさえあれば　ふたたび　平和をとりもどす　ことができるのだ　しかしどこに　まただれが　そのスタンプをさがしにいくのか？")
-
-
-typede("#typed-sample9","みんながそうおもったとき　3にんの　ゆうきあるせんしがたちあがった！！　そのなを　ゆうしゃ　ヒーラー　魔法使い　\nはたしてかれらは　ぶじ　スタンプを　さがしだすことができるのであろうか？")
-
-typede("#typed-sample11","3にんは　ふあんときぼうをむねにひめ　いま　たびだつことをけついした　勇敢なるせんしたちよ　ぜひこの高校をまもってくれ！！")
-
-
-document.addEventListener('DOMContentLoaded', function () {
-    var mosaicInElement = document.querySelector('#mosaic-in');
-    mosaicInElement.mosaicIn({
-        duration: 1500,
-        intervalFrame: 5
-    });
-});
-
-$(document).ready(function () {
-    $('#mosaic-in').mosaicIn({
-        duration: 1500,
-        intervalFrame: 5
-    });
-});
-
-$(document).ready(function () {
-    $('#member_in').mosaicIn({
-        duration: 12000,
-        intervalFrame: 5
-    });
-});
+}
+function type7in(){
+    typede("#typed-sample7","3にんは　ふあんときぼうをむねにひめ　いま　たびだつことをけついした!　勇敢なるせんしたちよ　ぜひこの国をまもってくれ！！")
 
 
-$(document).ready(function () {
-    $('.img_loop',).mosaicIn({
-        duration: 1200,
-        intervalFrame: 5
-    });
-});
+}
 
 
-$(document).ready(function () {
-    $('#stamp_type6',).mosaicIn({
-        duration: 1200,
-        intervalFrame: 5
+
+
+
+
+
+
+
+
+
+document.querySelectorAll(".btn").forEach(button => {
+    button.addEventListener("click", function () {
+        
+
+        // 質問5のボタンがクリックされた場合、結果を表示
+        if (this.classList.contains("end")) {
+            showResult();
+        } else {
+            // 質問の親要素を隠す
+            const parentDiv = this.closest("div");
+            console.log(parentDiv)
+            parentDiv.style.display = "none";
+
+            // 次の質問を表示
+            const id = this.getAttribute("href");
+            console.log(id)
+            const nextQuestion = document.querySelector(id);
+            nextQuestion.classList.add("fit");
+            nextQuestion.style.display = "block";
+            
+        }
     });
 });
